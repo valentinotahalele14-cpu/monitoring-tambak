b6ad39bb7de2a281c39a1b469741a30c879b318c
valentinotahalele14-cpu <valentino.tahalele14@gmail.com>
2026-08-04 00:06:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+import {
 
+    database
+
+} from "./firebase.js";
+
+import {
+
+    ref,
+
+    onValue
+
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
+
+const realtimeRef = ref(database, "monitoring/realtime");
+
+onValue(realtimeRef, (snapshot) => {
+
+    const data = snapshot.val();
+
+    if (!data) return;
+
+    document.getElementById("status").innerHTML =
+        "🟢 Online";
+
+    document.getElementById("update").innerHTML =
+        data.tanggal + "<br>" + data.jam;
+
+    document.getElementById("suhu1").innerHTML =
+        data.suhu1.toFixed(2) + " °C";
+
+    document.getElementById("suhu2").innerHTML =
+        data.suhu2.toFixed(2) + " °C";
+
+    document.getElementById("suhu3").innerHTML =
+        data.suhu3.toFixed(2) + " °C";
+
+});
